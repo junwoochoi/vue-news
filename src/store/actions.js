3836288efa1c0e062eb54328da17ac88d6280cc8
최준우 <junwoochoi@zuminternet.com>
2019-12-17 10:14:38 +0900

    refactor : separate store modules

---
+++ src/store/actions.js
@@ -0,0 +1,19 @@
+import { fetchNewsList, fetchAskList, fetchJobsList } from '../api';
+
+export default {
+  FETCH_NEWS(context){
+    fetchNewsList()
+    .then(res => context.commit('SET_NEWS', res.data))
+    .catch(err => console.error(err));
+  },
+  FETCH_JOBS(context){
+    fetchJobsList()
+    .then(res => context.commit('SET_JOBS', res.data))
+    .catch(err => console.error(err));
+  },
+  FETCH_ASK(context){
+    fetchAskList()
+    .then(res => context.commit('SET_ASK', res.data))
+    .catch(err => console.error(err));
+  },
+}
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import { fetchNewsList, fetchAskList, fetchJobsList } from '../api';
+import mutations from './mutations';
+import actions from './actions';
 
 Vue.use(Vuex);
 
@@ -10,37 +11,11 @@ export const store = new Vuex.Store({
     jobs: [],
     ask: []
   },
-  mutations: {
-    SET_NEWS(state, data) {
-      state.news = data;
-    },
-    SET_JOBS(state, data) {
-      state.jobs = data;
-    },
-    SET_ASK(state, data) {
-      state.ask = data;
-    },
-  },
   getters: {
     fetchedNews: (state) => state.news,
     fetchedJobs: (state) => state.jobs,
     fetchedAsk: (state) => state.ask,
   },
-  actions: {
-    FETCH_NEWS(context){
-      fetchNewsList()
-      .then(res => context.commit('SET_NEWS', res.data))
-      .catch(err => console.error(err));
-    },
-    FETCH_JOBS(context){
-      fetchJobsList()
-      .then(res => context.commit('SET_JOBS', res.data))
-      .catch(err => console.error(err));
-    },
-    FETCH_ASK(context){
-      fetchAskList()
-      .then(res => context.commit('SET_ASK', res.data))
-      .catch(err => console.error(err));
-    },
-  },
+  mutations,
+  actions
 })
