5879eccb87dedff9c6cef0bb831a5166239563fd
최준우 <junwoochoi@zuminternet.com>
2019-12-23 14:31:16 +0900

    feat: ListItem 공통 컴포넌트화

--- src/views/JobsView.vue
+++ src/views/JobsView.vue
@@ -1,68 +1,16 @@
 <template>
   <div>
-    <ul class="news-list">
-        <li v-for="item in fetchedJobs" class="post">
-          <div class="points">
-            {{item.points || 0}}
-          </div>
-          <div>
-            <p class="news-title">
-              <a :href="item.url">
-                {{ item.title }}
-              </a>
-            </p>
-            <small class="link-text">
-              {{ item.time_ago }} by
-              <a :href="item.url">{{ item.domain || 'empty' }}</a>
-            </small>
-          </div>
-        </li>
-      </ul>
+    <ListItem/>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import ListItem from '../components/ListItem';
+
 export default {
-  data(){
-    return {
-    }
-  },
-  computed:{
-    ...mapGetters([
-      'fetchedJobs'
-    ])
-  },
-  created(){
-    this.$store.dispatch('FETCH_JOBS')
+  components: {
+    ListItem
   }
 }
 </script>
 
-<style scoped>
-.news-list {
-  padding: 0;
-  margin: 0;
-}
-.post {
-  list-style: none;
-  display: flex;
-  align-items: center;
-  border-bottom: 1px solid #eee;
-}
-
-.points {
-  width: 80px;
-  height: 60px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  color: #43c083;
-}
-.news-title {
-  margin: 0;
-}
-.link-text {
-  color: #828282;
-}
-</style>
--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,67 +1,15 @@
 <template>
-  <div>
-      <ul class="news-list">
-        <li v-for="item in fetchedNews" class="post">
-          <div class="points">
-            {{item.points}}
-          </div>
-          <div>
-            <p class="news-title">
-              <a :href="item.url">
-              {{ item.title }}
-              </a>
-            </p>
-            <small class="link-text">
-              {{ item.time_ago }} by <router-link :to="`/user/${item.user}`">{{ item.user }}</router-link>
-            </small>
-          </div>
-        </li>
-      </ul>
-  </div>
+  <ListItem/>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import ListItem from '../components/ListItem';
+
+
 export default {
-  data(){
-    return {
-    }
-  },
-  computed:{
-    ...mapGetters([
-      'fetchedNews'
-    ])
-  },
-  created(){
-    this.$store.dispatch('FETCH_NEWS')
+  components: {
+    ListItem
   }
 }
 </script>
 
-<style scoped>
-.news-list {
-  padding: 0;
-  margin: 0;
-}
-.post {
-  list-style: none;
-  display: flex;
-  align-items: center;
-  border-bottom: 1px solid #eee;
-}
-
-.points {
-  width: 80px;
-  height: 60px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  color: #43c083;
-}
-.news-title {
-  margin: 0;
-}
-.link-text {
-  color: #828282;
-}
-</style>
